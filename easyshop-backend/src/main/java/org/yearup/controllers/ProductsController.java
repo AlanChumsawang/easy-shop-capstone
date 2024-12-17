64c63cdabba0fa3f492eccf31e209e4a7c5a3eb3
AlanChumsawang <alan.chumsawang@outlook.com>
2024-12-17 15:22:57 -0500

    Fixed getById product method

--- easyshop-backend/src/main/java/org/yearup/controllers/ProductsController.java
+++ easyshop-backend/src/main/java/org/yearup/controllers/ProductsController.java
@@ -26,7 +26,7 @@ public class ProductsController
 
     @GetMapping("")
     @PreAuthorize("permitAll()")
-    public List<Product> search(@RequestParam(name="cat", required = false) Integer categoryId,
+    public List<Product> search(@RequestParam(name="id", required = false) Integer categoryId,
                                 @RequestParam(name="minPrice", required = false) BigDecimal minPrice,
                                 @RequestParam(name="maxPrice", required = false) BigDecimal maxPrice,
                                 @RequestParam(name="color", required = false) String color
@@ -48,7 +48,7 @@ public class ProductsController
     {
         try
         {
-            var product = productDao.getById(id);
+            Product product = productDao.getById(id);
 
             if(product == null)
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
